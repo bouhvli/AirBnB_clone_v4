9b486b2e35861587ce4e653d6f7e4a08883be0a2
bouhvli <bouhali.hamza.2@gmail.com>
2023-11-05 10:22:11 +0100

    adding task 4, 5

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+    var idsOfAmenitiesSelected = {};
+    $('.amenities .popover input').change(function (e) { 
+        var amenityId = $(this).attr('data-id');
+        var amenityName = $(this).attr('data-name');
+        if ($(this).is(':checked')) {
+            idsOfAmenitiesSelected[amenityName] = amenityId;
+        } else {
+            delete idsOfAmenitiesSelected[amenityName];
+        }
+        var selectedNames = Object.keys(idsOfAmenitiesSelected);
+        if (selectedNames.length < 3) {
+            for (let i = 0; i < 3 && i < selectedNames.length; i++) {
+                $('.amenities h4').text(selectedNames.join(', '));
+            }
+        } else if (selectedNames.length == 3) {
+            $('.amenities h4').text(selectedNames.concat(' ...'));
+        }
+    });
+    $.get("http://localhost:5001/api/v1/status/",
+        function (data, textStatus) {
+            console.log(textStatus)
+            if (data.status === 'OK' && textStatus === 'success')
+                $('div#api_status').addClass('available');
+            else
+                $('div#api_status').removeClass('available');
+        }
+    );
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:5001/api/v1/places_search/",
+        data: JSON.stringify({}),
+        headers: { 'Content-Type': 'application/json' },
+        success: function (response) {
+            for (const place of response) {
+                const articleTag = [
+                    '<article>',
+                        '<div class="title_box">',
+                        `<h2>${place.name}</h2>`,
+                        `<div class="price_by_night">$${place.price_by_night}</div>`,
+                        '</div>',
+                        '<div class="information">',
+                        `<div class="max_guest">${place.max_guest}</div>`,
+                        `<div class="number_rooms">${place.number_rooms}</div>`,
+                        `<div class="number_bathrooms">${place.number_bathrooms}</div>`,
+                        '</div>',
+                        '<div class="description">',
+                        `${place.description}`,
+                        '</div>',
+                    '</article>'
+                ];
+                $('SECTION.places').append(articleTag.join(''));
+            }
+        },
+        error: function (e) {
+            console.log(e)
+        }
+    });
+});
